9fa27c9f8fa36261151c6f84e96d2b7a8d7ad15f
tanga <tanga@etlchina.net>
2020-05-12 15:18:55 +0800

    首次上传

--- app/src/main/java/com/tangao/tapermission/MainActivity.kt
+++ app/src/main/java/com/tangao/tapermission/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.widget.Toast
 import com.tangao.library.TAPermission
 import kotlinx.android.synthetic.main.activity_main.*
+
 /**
  *    author : TANGAO
  *    e-mail : TAA1007@163.com
@@ -20,16 +21,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btn.setOnClickListener {
-            TAPermission.request(this , Manifest.permission.CALL_PHONE , Manifest.permission.WRITE_EXTERNAL_STORAGE){ allGranted, deniedList ->
-            if (allGranted){
-                call()
-            }else{
-                Toast.makeText(this, "You  Denied $deniedList", Toast.LENGTH_SHORT).show()
+            /*
+            * 支持一次性请求多个权限
+            * */
+            TAPermission.request(
+                this,
+                Manifest.permission.CALL_PHONE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) { allGranted, deniedList ->
+                if (allGranted) {
+                    call()
+                } else {
+                    Toast.makeText(this, "You  Denied $deniedList", Toast.LENGTH_SHORT).show()
+                }
             }
-        } }
+        }
 
     }
-    private fun call(){
+
+    private fun call() {
         val intent = Intent(Intent.ACTION_CALL)
         intent.data = Uri.parse("tel:10086")
         startActivity(intent)
